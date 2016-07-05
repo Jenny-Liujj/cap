938fd7c4a3d9d759075a629a11ea634d5c5a7532
刘精精 <liujingjing7899@gmail.com>
2016-07-05 09:56:31 +0800

    first commit
    first commit

---
+++ config/deploy.rb
@@ -0,0 +1,20 @@
+lock '3.4.1'
+
+set :application, 'cap-work'
+set :repo_url, 'https://liu7899@github.com/liu7899/cap-work.git'
+
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :deploy_to, '/home/ljj/cap-work'
+set :scm, :git
+
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+set :keep_releases, 15
+
+namespace :deploy do
+
+  after :publishing, 'deploy:restart'
+  after :finishing, 'deploy:cleanup'
+
+end
